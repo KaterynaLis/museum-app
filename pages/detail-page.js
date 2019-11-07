c537eca5fcfc122dd0273e569bea3dd8747bc496
KaterynaLis <votetolisitsa@gmail.com>
2019-11-07 13:58:58 +0100

    add section h3 and p elements

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,10 +1,19 @@
 function submitComment(){
     const inputField = document.getElementById("name")
     const name = inputField.value
-    console.log(name)
+        console.log(name)
 
     const textArea = document.getElementById("msg")
     const message = textArea.value
-    console.log(message)
+        console.log(message)
+
+    const comment = document.createElement('section')
+        console.log(comment)
+
+    const h3 = document.createElement('h3')
+        console.log(h3)    
+
+    const p = document.createElement('p')
+        console.log(p)  
 }
 
