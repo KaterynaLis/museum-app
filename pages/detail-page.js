708efb9bb832a90d77047c7d2cb0d25535042223
KaterynaLis <votetolisitsa@gmail.com>
2019-11-07 13:50:27 +0100

    collecting data from message fild

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -2,5 +2,9 @@ function submitComment(){
     const inputField = document.getElementById("name")
     const name = inputField.value
     console.log(name)
+
+    const textArea = document.getElementById("msg")
+    const message = textArea.value
+    console.log(message)
 }
 
